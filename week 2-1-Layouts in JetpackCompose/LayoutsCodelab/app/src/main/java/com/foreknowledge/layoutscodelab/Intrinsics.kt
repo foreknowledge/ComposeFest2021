a36bfa780128da369712897caea696f53935bda3
foreknowledge <kyj_0129@naver.com>
2021-11-14 00:22:16 +0900

    Week2: Intrinsics

---
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/foreknowledge/layoutscodelab/Intrinsics.kt
@@ -0,0 +1,49 @@
+package com.foreknowledge.layoutscodelab
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.foreknowledge.layoutscodelab.ui.theme.LayoutsCodelabTheme
+
+/**
+ * Created by Yeji on 2021/11/14.
+ */
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier.fillMaxHeight().width(1.dp))
+
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TwoTextsPreview() {
+    LayoutsCodelabTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
+}
